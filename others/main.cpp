62fcf1c1f4adb5fb77ad106c7a4b461a58bef01b
Orbdiff <160801916+Orbdiff@users.noreply.github.com>
2025-09-15 17:32:56 -0500

    update (jarparser.exe)

---
+++ others/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+#include "usn_reader.hh"
+
+std::map<ULONGLONG, std::wstring> pendingRenames;
+
+
+int wmain()
+{
+    std::wstring volumeLetter = L"C:";
+
+    USNJournalReader reader(volumeLetter);
+    return reader.Run();
+}
